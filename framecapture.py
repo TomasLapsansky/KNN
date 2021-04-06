945058e8347e34c62e45740ff0756ab0d9703c67
filipplesko <xplesk02@stud.fit.vutbr.cz>
2021-04-06 22:50:38 +0200

    crop caras

--- framecapture.py
+++ framecapture.py
@@ -1,25 +1,53 @@
 import cv2
-import pandas as pd 
+import pandas as pd
 import os
 import csv
+
+videos = {}
 
 
 def cutframe(car,video,count,image):
      # for car in frame:
     start_point = (int(car['UpperPointLongX']),int(car['UpperPointLongY']))
     end_point =  (int(car['ShortSideX']),int(car['ShortSideY']))
-    image = image[start_point[1]:end_point[1], start_point[0]:end_point[0]]
+
+    pointsX = [int(car['UpperPointShortX']), int(car['UpperPointCornerX']), int(car['UpperPointLongX']),
+    int(car['CrossCornerX']), int(car['ShortSideX']), int(car['CornerX']), int(car['LongSideX']), int(car['LowerCrossCornerX'])]
+    pointsY = [int(car['UpperPointShortY']), int(car['UpperPointCornerY']), int(car['UpperPointLongY']),
+    int(car['CrossCornerY']), int(car['ShortSideY']), int(car['CornerY']), int(car['LongSideY']), int(car['LowerCrossCornerY'])]
+    minX = min(pointsX)
+    minY = min(pointsY)
+    maxX = max(pointsX)
+    maxY = max(pointsY)
+    if (maxY - minY < 70 or maxX - minX < 70): #remove small pics
+        return
+
+    # image = cv2.circle(image, (int(car['UpperPointShortX']),int(car['UpperPointShortY'])), radius=10, color=(0, 0, 255), thickness=-1)
+    # image = cv2.circle(image, (int(car['UpperPointCornerX']),int(car['UpperPointCornerY'])), radius=10, color=(0, 0, 255), thickness=-1)
+    # image = cv2.circle(image, (int(car['UpperPointLongX']),int(car['UpperPointLongY'])), radius=10, color=(0, 0, 255), thickness=-1)
+    # image = cv2.circle(image, (int(car['CrossCornerX']),int(car['CrossCornerY'])), radius=10, color=(0, 0, 255), thickness=-1)
+    # image = cv2.circle(image, (int(car['ShortSideX']),int(car['ShortSideY'])), radius=10, color=(0, 0, 255), thickness=-1)
+    # image = cv2.circle(image, (int(car['CornerX']),int(car['CornerY'])), radius=10, color=(0, 0, 255), thickness=-1)
+    # image = cv2.circle(image, (int(car['LongSideX']),int(car['LongSideY'])), radius=10, color=(0, 0, 255), thickness=-1)
+    # image = cv2.circle(image, (int(car['LowerCrossCornerX']),int(car['LowerCrossCornerY'])), radius=10, color=(0, 0, 255), thickness=-1)
+
+    crop_image = image[minY:maxY, minX:maxX]
 
     video_name = str(((os.path.splitext(video)[0]).split("/"))[-1])
     car_id = int(car['car_id'])
     file_name = "capt/" + str(video_name)+ "_id_" + str(car_id) + "_frame_" + str(count) + ".jpg"
+
+
     try:
-        cv2.imwrite(file_name, image)     # save frame as JPEG file      
+        cv2.imwrite(file_name, crop_image)     # save frame as JPEG file
     except:
-        print("ERROR")
-    print("New image captured",count)
-    # except:
-    #     print("ops")
+        return
+
+    if video in videos:
+        videos[video] = videos[video] + 1
+    else:
+        videos[video] = 1
+    
 
 def video_capt(df,video):
     vidcap = cv2.VideoCapture(video)
@@ -59,6 +87,9 @@ def main():
             csv_name = "dataset/video_shots/" + str(os.path.splitext(filename)[0]) + "_annotations.csv"
             df = pd.read_csv(csv_name)
             video_capt(df,file_name)
+
+    for video, cnt in videos.items():
+        print(video, cnt)
 
 if __name__ == "__main__":
     main()

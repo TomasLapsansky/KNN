e7654eb733de6fb9a0d2e067eddb56c746ee5104
filipplesko <xplesk02@stud.fit.vutbr.cz>
2021-04-06 20:41:43 +0200

    tmp

--- framecapture.py
+++ framecapture.py
@@ -1,31 +1,47 @@
 import cv2
 import pandas as pd 
+import os
+import csv
+
+
+def cutframe(car,video,count,image):
+     # for car in frame:
+    start_point = (int(car['UpperPointLongX']),int(car['UpperPointLongY']))
+    end_point =  (int(car['ShortSideX']),int(car['ShortSideY']))
+    image = image[start_point[1]:end_point[1], start_point[0]:end_point[0]]
+
+    video_name = str(((os.path.splitext(video)[0]).split("/"))[-1])
+    car_id = int(car['car_id'])
+    file_name = "capt/" + str(video_name)+ "_id_" + str(car_id) + "_frame_" + str(count) + ".jpg"
+    try:
+        cv2.imwrite(file_name, image)     # save frame as JPEG file      
+    except:
+        print("ERROR")
+    print("New image captured",count)
+    # except:
+    #     print("ops")
 
 def video_capt(df,video):
-    vidcap = cv2.VideoCapture('1A.mov')
+    vidcap = cv2.VideoCapture(video)
     success,image = vidcap.read()
-    count = 0
+    count = 3 #fix offset so anotation is correct
     while success:
 
         if((df['frame'] == count).any()):
 
             
             frame = df.loc[df['frame'] == count]
-
-            startpoint = (frame['UpperPointLongX'],frame['UpperPointLongY'])
-            end_point =  (frame['ShortSideX'],frame['ShortSideY'])
-            color = (255, 0, 0)
-            thickness = 2
-            try:
-                image = cv2.rectangle(image, startpoint, end_point, color, thickness)
+            # try:
+            
+            if(len(frame)==1):
+                cutframe(frame,video,count,image)
+            else:
+                print(frame.iloc(0))
+                cutframe(frame.iloc(0),video,count,image)
                 
+
                 
-
-
-                cv2.imwrite("capt/frame%d.jpg" % count, image)     # save frame as JPEG file      
-                print("New image captured",count)
-            except:
-                print("ops")
+           
             
         success,image = vidcap.read()
         
@@ -37,10 +53,13 @@ def video_capt(df,video):
 
 
 def main():
-    df = pd.read_csv('table.csv')
-    print(df)
 
-    video_capt(df,'sample.mp4')
+    for filename in os.listdir("dataset/video_shots/"):
+        if filename.endswith(".mov"):
+            file_name = (os.path.join("dataset/video_shots/", filename))
+            csv_name = "dataset/video_shots/" + str(os.path.splitext(filename)[0]) + "_annotations.csv"
+            df = pd.read_csv(csv_name)
+            video_capt(df,file_name)
 
 if __name__ == "__main__":
     main()

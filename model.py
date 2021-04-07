78627ea9bee7ea83276352a978d07937616e0b74
Tomas Lapsansky <tomas.lapsansky.97@gmail.com>
2021-04-07 20:36:13 +0200

    refactor

--- model.py
+++ model.py
@@ -9,7 +9,6 @@ from keras import Model
 from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import Adam, SGD
-from keras.utils import to_categorical
 from keras.callbacks import ModelCheckpoint
 from tensorflow.keras import regularizers
 from keras.layers.core import Lambda
@@ -22,11 +21,11 @@ import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-
 import random, os
 from sklearn.model_selection import train_test_split
 
-INPUT_SHAPE = (128,128,3)
+INPUT_SHAPE = (128, 128, 3)
+
 
 def get_pair(camera, id):
     try:
@@ -38,9 +37,7 @@ def get_pair(camera, id):
         return "none/none"
 
 
-
-def create_pair(images,batch_size,positive):
-
+def create_pair(images, batch_size, positive):
     output = []
 
     pairImg = []
@@ -51,136 +48,121 @@ def create_pair(images,batch_size,positive):
     width, height, rest = INPUT_SHAPE
 
     i = 0
-    while(i < batch_size):
+    while (i < batch_size):
         print(i, batch_size, end='\r')
         image = random.choice([x for x in os.listdir(pathA)
-            if os.path.isfile(os.path.join(pathA, x))])
-        if(image[1]=="B"):
+                               if os.path.isfile(os.path.join(pathA, x))])
+        if (image[1] == "B"):
             continue
         imagelist = image.split("_")
-        pair = get_pair(int(imagelist[0][0]),int(imagelist[2]))
+        pair = get_pair(int(imagelist[0][0]), int(imagelist[2]))
         pair = pair.split("/")
-        prefix = pair[0][0]+"B_id_"+pair[1]
+        prefix = pair[0][0] + "B_id_" + pair[1]
         set_list = []
 
-        if(random.choice([True, False])):
+        if (random.choice([True, False])):
             for file in os.listdir(pathB):
                 if file.startswith(prefix):
                     set_list.append(file)
-            if(set_list == []):
+            if (set_list == []):
                 continue
-           
-            img1=image
-            img2=random.choice(set_list)
+
+            img1 = image
+            img2 = random.choice(set_list)
             labels.append([1])
         else:
-            
-            img1=image
-            img2=random.choice([x for x in os.listdir(pathB)
-                if os.path.isfile(os.path.join(pathB, x))])
+
+            img1 = image
+            img2 = random.choice([x for x in os.listdir(pathB)
+                                  if os.path.isfile(os.path.join(pathB, x))])
             labels.append([0])
 
-        img1 = cv2.imread(pathA+"/"+img1)
-        img1 = cv2.resize(img1,(width, height) , interpolation = cv2.INTER_AREA)
+        img1 = cv2.imread(pathA + "/" + img1)
+        img1 = cv2.resize(img1, (width, height), interpolation=cv2.INTER_AREA)
 
-        
-        
-        img2 = cv2.imread(pathB+"/"+img2)
-        img2 = cv2.resize(img2,(width, height) , interpolation = cv2.INTER_AREA)
-        
-        output.append([img1,img2])
-        
+        img2 = cv2.imread(pathB + "/" + img2)
+        img2 = cv2.resize(img2, (width, height), interpolation=cv2.INTER_AREA)
 
-        
+        output.append([img1, img2])
+
         i = i + 1
 
-        
-
-
-    return (np.array(output),np.array(labels))
-            
-        
-
-
-
+    return (np.array(output), np.array(labels))
 
 
 def initialize_weights(shape, dtype=None):
-  return np.random.normal(loc = 0.0, scale = 1e-2, size = shape)
+    return np.random.normal(loc=0.0, scale=1e-2, size=shape)
+
 
 def initialize_bias(shape, dtype=None):
-  return np.random.normal(loc = 0.5, scale = 1e-2, size = shape)
+    return np.random.normal(loc=0.5, scale=1e-2, size=shape)
 
 
-def create_model(input_shape=(128,128,1)):
-    
+def create_model(input_shape=(128, 128, 1)):
     # Define the tensors for the two input images
     left_input = Input(input_shape)
     right_input = Input(input_shape)
-    
+
     # Convolutional Neural Network
     model = Sequential()
-    model.add(Conv2D(64, (10,10), activation='relu', input_shape=input_shape,
-                    kernel_regularizer=l2(2e-4)))
+    model.add(Conv2D(64, (10, 10), activation='relu', input_shape=input_shape,
+                     kernel_regularizer=l2(2e-4)))
     model.add(MaxPooling2D())
-    model.add(Conv2D(128, (7,7), activation='relu',
+    model.add(Conv2D(128, (7, 7), activation='relu',
                      bias_initializer=initialize_bias, kernel_regularizer=l2(2e-4)))
     model.add(MaxPooling2D())
-    model.add(Conv2D(128, (4,4), activation='relu',
+    model.add(Conv2D(128, (4, 4), activation='relu',
                      bias_initializer=initialize_bias, kernel_regularizer=l2(2e-4)))
     model.add(MaxPooling2D())
-    model.add(Conv2D(256, (4,4), activation='relu',
+    model.add(Conv2D(256, (4, 4), activation='relu',
                      bias_initializer=initialize_bias, kernel_regularizer=l2(2e-4)))
     model.add(Flatten())
     model.add(Dense(4096, activation='sigmoid',
-                   kernel_regularizer=l2(1e-3),
-                   bias_initializer=initialize_bias))
-    
+                    kernel_regularizer=l2(1e-3),
+                    bias_initializer=initialize_bias))
+
     # Generate the encodings (feature vectors) for the two images
     encoded_l = model(left_input)
     encoded_r = model(right_input)
-    
+
     # Add a customized layer to compute the absolute difference between the encodings
-    L1_layer = Lambda(lambda tensors:K.abs(tensors[0] - tensors[1]))
+    L1_layer = Lambda(lambda tensors: K.abs(tensors[0] - tensors[1]))
     L1_distance = L1_layer([encoded_l, encoded_r])
-    
+
     # Add a dense layer with a sigmoid unit to generate the similarity score
-    prediction = Dense(1,activation='sigmoid',bias_initializer=initialize_bias)(L1_distance)
-    
+    prediction = Dense(1, activation='sigmoid', bias_initializer=initialize_bias)(L1_distance)
+
     # Connect the inputs with the outputs
-    siamese_net = Model(inputs=[left_input,right_input],outputs=prediction)
-    
+    siamese_net = Model(inputs=[left_input, right_input], outputs=prediction)
+
     # return the model
 
-    #siamese_net.summary()
+    # siamese_net.summary()
 
     return siamese_net
 
 
 def main():
-    
     EPOCHS = 100
     BATCH_SIZE = 16
     SPE = 100
     no_output = 2
-    
+
     model = create_model(input_shape=INPUT_SHAPE)
 
     opt = SGD(learning_rate=0.01, momentum=0.0, nesterov=False)
-    model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=["accuracy"])    
-    
-    generatorOut = create_pair(1,2000,True)
+    model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=["accuracy"])
+
+    generatorOut = create_pair(1, 2000, True)
     pairTrain, labelTrain = generatorOut
     pairTest, labelTest = generatorOut
 
-
     history = model.fit(
-	    [pairTrain[:, 0],pairTrain[:, 1]], labelTrain[:],
-	    validation_data=([pairTrain[:, 0],pairTrain[:, 1]], labelTrain[:]),
-	    batch_size=BATCH_SIZE,
-	    epochs=EPOCHS)
+        [pairTrain[:, 0], pairTrain[:, 1]], labelTrain[:],
+        validation_data=([pairTrain[:, 0], pairTrain[:, 1]], labelTrain[:]),
+        batch_size=BATCH_SIZE,
+        epochs=EPOCHS)
 
-    
 
 if __name__ == "__main__":
     main()

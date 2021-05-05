f980621838023a2253a893cacb9c053a05bd8540
minosalko <minosalko@gmail.com>
2021-05-05 22:30:10 +0200

    Loss possible fix

--- model.py
+++ model.py
@@ -85,15 +85,30 @@ T_G_SEED = 1337
 
 def triplet_hard_loss(y_true, y_pred):
     
+    ######################################################################################
+    
+    embeddings = y_pred
+    anchor_positive = embeddings[0]+embeddings[1]
+    negative = embeddings[2]
 
 
-    anchor = y_pred[0]
-    pos    = y_pred[1]
-    neg    = y_pred[2]
-    pos_dist   = tf.reduce_sum(tf.square(tf.subtract(anchor, pos)), axis=-1)    
-    neg_dist   = tf.reduce_sum(tf.square(tf.subtract(anchor, neg)), axis=-1)    
-    basic_loss = tf.subtract(pos_dist, neg_dist)    
-    loss       = tf.reduce_sum(tf.maximum(basic_loss, 0.0))               
+    # Compute pairwise distance between all of anchor-positive
+    dot_product = K.dot(anchor_positive, K.transpose(anchor_positive))
+    square = K.square(anchor_positive)
+    a_p_distance = K.reshape(K.sum(square, axis=1), (-1,1)) - 2.*dot_product  + K.sum(K.transpose(square), axis=0) + 1e-6
+    a_p_distance = K.maximum(a_p_distance, 0.0) ## Numerical stability
+
+
+    # Compute distance between anchor and negative
+    dot_product_2 = K.dot(anchor_positive, K.transpose(negative))
+    negative_square = K.square(negative)
+    a_n_distance = K.reshape(K.sum(square, axis=1), (-1,1)) - 2.*dot_product_2  + K.sum(K.transpose(negative_square), axis=0)  + 1e-6
+    a_n_distance = K.maximum(a_n_distance, 0.0) ## Numerical stability
+    
+    hard_negative = K.reshape(K.min(a_n_distance, axis=1), (-1, 1))
+    
+    distance = (a_p_distance - hard_negative + 0.2)
+    loss = K.mean(K.maximum(distance, 0.0))/(2.)     
         
     return loss
 

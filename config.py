294b7f442652c5b961f47651e5755e8109983569
minosalko <minosalko@gmail.com>
2021-05-06 00:35:13 +0200

    Check where run

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 INPUT_SHAPE = (224, 224, 3)
 
 EPOCHS = 50
@@ -5,6 +7,11 @@ BATCH_SIZE = 64
 SPE = 100
 VSTEPS = 20
 
-VERI_DATASET = './dataset/VeRi/'
-#VERI_DATASET = '/scratch.ssd/xlapsa00/job_7468248.meta-pbs.metacentrum.cz' + '/dataset/VeRi/'
+if(os.path.exists('/scratch.ssd/xlapsa00/job_7468248.meta-pbs.metacentrum.cz' + '/dataset/VeRi/')):
+    VERI_DATASET = '/scratch.ssd/xlapsa00/job_7468248.meta-pbs.metacentrum.cz' + '/dataset/VeRi/'
+else:
+    VERI_DATASET = './dataset/VeRi/'
+
+
+
 AIC_DATASET = './dataset/AIC21_Track2_ReID/'

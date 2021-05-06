badfa0b4c0c4c6214899a3722c352d596304ef1b
Tomas Lapsansky <tomas.lapsansky.97@gmail.com>
2021-05-07 00:33:20 +0200

    update

--- model.py
+++ model.py
@@ -89,8 +89,10 @@ def euclidean_distance(vects):
     x, y = vects
     return K.sqrt(K.maximum(K.sum(K.square(x - y), axis=1, keepdims=True), K.epsilon()))
 
+
+# https://stackoverflow.com/questions/52453079/what-accuracy-function-is-used-in-keras-when-using-metrics-accuracy
 def accuracy(y_true, y_pred):
-    return K.mean(y_pred[:,0,0] < y_pred[:,1,0])
+    return K.mean(K.equal(y_true, K.round(y_pred)), axis=-1)
 
 def create_model():
 

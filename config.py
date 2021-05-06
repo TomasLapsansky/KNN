d5c5794a316a705ffa9d59897a2f1d229d9bffa9
Tomas Lapsansky <tomas.lapsansky.97@gmail.com>
2021-05-06 17:28:10 +0200

    config for metacenter

--- config.py
+++ config.py
@@ -10,17 +10,18 @@ VSTEPS = 20
 
 try:
     print("Scratchdir exists")
-    scratchdir = os.environ["$SCRATCHDIR"]
+    scratchdir = os.popen('echo $SCRATCHDIR').read()
+    scratchdir = scratchdir[:-1]
     if not os.path.exists(scratchdir + '/dataset/VeRi/'):
         print("Copying dataset to scratchdir")
         os.mkdir(scratchdir + '/dataset')
         shutil.copytree('./dataset/VeRi', scratchdir + '/dataset/VeRi')
         print("Copying finished")
+    else:
+        print("Dataset exists")
     VERI_DATASET = scratchdir + '/dataset/VeRi/'
 except:
     print("Scratchdir doesn't exist")
     VERI_DATASET = './dataset/VeRi/'
-
-
 
 AIC_DATASET = './dataset/AIC21_Track2_ReID/'

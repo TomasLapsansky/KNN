d6b616234fb0876c178b9ac89a5408389edffbcf
Tomas Lapsansky <tomas.lapsansky.97@gmail.com>
2021-05-07 10:03:52 +0200

    warnings update

--- model.py
+++ model.py
@@ -12,10 +12,6 @@ from keras.models import Model
 
 import keras.layers as kl
 
-
-
-
-
 import xml.etree.ElementTree as ET
 import numpy as np
 
@@ -29,17 +25,12 @@ import os
 import config
 import generator
 
-
-
 # My generator
-import generator 
-
+import generator
 
 SN = 3
 PN = 24
 identity_num = 751
-
-
 
 # TRENOVANIE A VYTVORENIE SETU
 # https://github.com/noelcodella/tripletloss-keras-tensorflow/blob/master/tripletloss.py
@@ -48,40 +39,39 @@ identity_num = 751
 # https://github.com/michuanhaohao/keras_reid/blob/master/reid_tripletcls.py
 
 
-#Global pre generatory
+# Global pre generatory
 
 
-
-T_G_HEIGHT,T_G_WIDTH = 224, 224
+T_G_HEIGHT, T_G_WIDTH = 224, 224
 T_G_SEED = 1337
 
-def triplet_hard_loss(y_true, y_pred):
-    
-    ######################################################################################
-    
-    embeddings = y_pred
-    anchor_positive = embeddings[0]+embeddings[1]
-    negative = embeddings[2]
 
+def triplet_hard_loss(y_true, y_pred):
+    ######################################################################################
+
+    embeddings = y_pred
+    anchor_positive = embeddings[0] + embeddings[1]
+    negative = embeddings[2]
 
     # Compute pairwise distance between all of anchor-positive
     dot_product = K.dot(anchor_positive, K.transpose(anchor_positive))
     square = K.square(anchor_positive)
-    a_p_distance = K.reshape(K.sum(square, axis=1), (-1,1)) - 2.*dot_product  + K.sum(K.transpose(square), axis=0) + 1e-6
-    a_p_distance = K.maximum(a_p_distance, 0.0) ## Numerical stability
-
+    a_p_distance = K.reshape(K.sum(square, axis=1), (-1, 1)) - 2. * dot_product + K.sum(K.transpose(square),
+                                                                                        axis=0) + 1e-6
+    a_p_distance = K.maximum(a_p_distance, 0.0)  ## Numerical stability
 
     # Compute distance between anchor and negative
     dot_product_2 = K.dot(anchor_positive, K.transpose(negative))
     negative_square = K.square(negative)
-    a_n_distance = K.reshape(K.sum(square, axis=1), (-1,1)) - 2.*dot_product_2  + K.sum(K.transpose(negative_square), axis=0)  + 1e-6
-    a_n_distance = K.maximum(a_n_distance, 0.0) ## Numerical stability
-    
+    a_n_distance = K.reshape(K.sum(square, axis=1), (-1, 1)) - 2. * dot_product_2 + K.sum(K.transpose(negative_square),
+                                                                                          axis=0) + 1e-6
+    a_n_distance = K.maximum(a_n_distance, 0.0)  ## Numerical stability
+
     hard_negative = K.reshape(K.min(a_n_distance, axis=1), (-1, 1))
-    
+
     distance = (a_p_distance - hard_negative + 0.2)
-    loss = K.mean(K.maximum(distance, 0.0))/(2.)     
-        
+    loss = K.mean(K.maximum(distance, 0.0)) / (2.)
+
     return loss
 
 
@@ -90,37 +80,33 @@ def euclidean_distance(vects):
     return K.sqrt(K.maximum(K.sum(K.square(x - y), axis=1, keepdims=True), K.epsilon()))
 
 
-# https://stackoverflow.com/questions/47692541/implementing-accuracy-for-triplet-loss-in-keras
-def accuracy(_, y_pred):
-    subtraction = K.constant([-1, 1], shape=(2, 1))
-    diff = K.dot(y_pred, subtraction)
-    loss = K.maximum(K.sign(diff), K.constant(0))
+def accuracy(y_true, y_pred):
+    return K.mean(y_pred[:, 0, 0] < y_pred[:, 1, 0])
 
-    return loss
 
 def create_model():
-
-    emb_size = 776 # number of classes in dataset
+    emb_size = 776  # number of classes in dataset
 
     print('Creating a model ...')
 
-     # Initialize a ResNet50_ImageNet Model
+    # Initialize a ResNet50_ImageNet Model
     resnet_input = kl.Input(shape=config.INPUT_SHAPE)
-    resnet_model = keras.applications.resnet50.ResNet50(weights='imagenet', include_top = False, input_tensor=resnet_input)
+    resnet_model = keras.applications.resnet50.ResNet50(weights='imagenet', include_top=False,
+                                                        input_tensor=resnet_input)
 
     # New Layers over ResNet50
     net = resnet_model.output
-    #net = kl.Flatten(name='flatten')(net)
+    # net = kl.Flatten(name='flatten')(net)
     net = kl.GlobalAveragePooling2D(name='gap')(net)
-    #net = kl.Dropout(0.5)(net)
-    net = kl.Dense(emb_size,activation='relu',name='t_emb_1')(net)
-    net = kl.Lambda(lambda  x: K.l2_normalize(x,axis=1), name='t_emb_1_l2norm')(net)
+    # net = kl.Dropout(0.5)(net)
+    net = kl.Dense(emb_size, activation='relu', name='t_emb_1')(net)
+    net = kl.Lambda(lambda x: K.l2_normalize(x, axis=1), name='t_emb_1_l2norm')(net)
 
     # model creation
     base_model = Model(resnet_model.input, net, name="base_model")
 
     # triplet framework, shared weights
-    input_shape=config.INPUT_SHAPE
+    input_shape = config.INPUT_SHAPE
     input_anchor = kl.Input(shape=input_shape, name='input_anchor')
     input_positive = kl.Input(shape=input_shape, name='input_pos')
     input_negative = kl.Input(shape=input_shape, name='input_neg')
@@ -135,10 +121,11 @@ def create_model():
     tertiary_dist = kl.Lambda(euclidean_distance, name='ter_dist')([net_positive, net_negative])
 
     # This lambda layer simply stacks outputs so both distances are available to the objective
-    stacked_dists = kl.Lambda(lambda vects: K.stack(vects, axis=1), name='stacked_dists')([positive_dist, negative_dist, tertiary_dist])
+    stacked_dists = kl.Lambda(lambda vects: K.stack(vects, axis=1), name='stacked_dists')(
+        [positive_dist, negative_dist, tertiary_dist])
 
     model = Model([input_anchor, input_positive, input_negative], stacked_dists, name='triple_siamese')
-    
+
     # Setting up optimizer designed for variable learning rate
 
     for layer in model.layers:
@@ -149,7 +136,6 @@ def create_model():
     model.compile(loss=triplet_hard_loss, optimizer=optim, metrics=[accuracy])
     model.summary()
 
-
     return model
 
 
@@ -157,60 +143,37 @@ def main():
     # tensorflow devices (GPU) print
     # print(device_lib.list_local_devices())
 
-    print("Version of Tensor Flow:",tf.__version__)
-    
-    print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+    print("Version of Tensor Flow:", tf.__version__)
 
+    print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
     model = create_model()
 
-    print("\n"*5)
+    print("\n" * 5)
 
     path = config.VERI_DATASET + 'train_label.xml'
     batch = config.BATCH_SIZE
-    lenitem = batch  
+    lenitem = batch
 
     gen = generator.MyGenerator(path, batch, lenitem)
-    SPE = len(gen.Y_train)/config.EPOCHS
+    SPE = len(gen.Y_train) / config.EPOCHS
     print(SPE)
-    
-    model.fit_generator(generator=gen.localSet(), steps_per_epoch= config.SPE, epochs=config.EPOCHS, shuffle=False, use_multiprocessing=True)
 
-       
+    # model.fit_generator(generator=gen.localSet(), steps_per_epoch= config.SPE, epochs=config.EPOCHS, shuffle=False, use_multiprocessing=True)
+    # https://keunwoochoi.wordpress.com/2017/08/24/tip-fit_generator-in-keras-how-to-parallelise-correctly/
+    model.fit(gen.localSet(),
+              steps_per_epoch=config.SPE,
+              epochs=config.EPOCHS,
+              shuffle=False,
+              use_multiprocessing=False)
+
     exit(0)
 
-    
-    
+    ########################################## NACITAVANIE XML
 
-    
+    ##########################################
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-########################################## NACITAVANIE XML
-    
-    
-##########################################
-
-    
-    
     exit(1)
-
-
-    
 
     print("Start training ")
     print("Loading files...")

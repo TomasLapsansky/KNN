989a59fc0db76e1ee1ba68c027bbbe7489d430e7
filipplesko <xplesk02@stud.fit.vutbr.cz>
2021-05-07 12:49:11 +0200

    fix mem

--- generator.py
+++ generator.py
@@ -7,48 +7,43 @@ import keras
 from keras.preprocessing.image import ImageDataGenerator
 import random
 
-
-
 T_G_SEED = 1337
+
 
 class MyGenerator():
 
-    def __init__(self,path, batch, lenitem):
+    def __init__(self, path, batch, lenitem):
         self.df = self.loadXML(path)
-        self.batch = batch
         self.lenItem = lenitem
-        self.anchor = []
-        self.positive = []
-        self.negative = []
-        self.Y_train = []
 
         self.datagen = ImageDataGenerator(width_shift_range=0.05,
-                                 height_shift_range=0.05,
-                                 zoom_range=0.1,
-                                 vertical_flip=True,
-                                )
+                                          height_shift_range=0.05,
+                                          zoom_range=0.1,
+                                          vertical_flip=True,
+                                          )
 
-    def loadXML(self,path):
+    def loadXML(self, path):
         print('loading label xml...', end="")
         xml_data = open(path, 'r').read()  # Read file
-        root = ET.XML(xml_data) 
+        root = ET.XML(xml_data)
 
         data = []
         cols = []
-        items=root[0]
+        items = root[0]
 
         for i, item in enumerate(items):
-            data.append([item.attrib['imageName'],item.attrib['vehicleID']])
+            data.append([item.attrib['imageName'], item.attrib['vehicleID']])
 
         df = pd.DataFrame(data)  # Write in DF and transpose it
         df.columns = ["imageName", "vehicleID"]  # Update column names
         print("DONE")
         return df
 
-    def load_img(self,p2f):
+    @staticmethod
+    def load_img(p2f):
 
         height, width, _ = config.INPUT_SHAPE
-        
+
         t_image = cv2.imread(p2f)
         t_image = cv2.resize(t_image, (height, width))
         t_image = t_image.astype("float32")
@@ -56,30 +51,29 @@ class MyGenerator():
         return t_image
 
     def localSet(self):
-
-        output = []
-        labels = []
-        dirc=config.VERI_DATASET+"image_train/"
+        dirc = config.VERI_DATASET + "image_train/"
         while True:
+            output = []
+            labels = []
             for i in range(self.lenItem):
 
                 # Load random sample from data frame
-                row = self.df.sample()          
+                row = self.df.sample()
 
                 # Load car ID and image name for anchor      
-                car_A, name_A = row['vehicleID'].values[0], row['imageName'].values[0] 
-                
+                car_A, name_A = row['vehicleID'].values[0], row['imageName'].values[0]
+
                 # Load car ID and image name for positive
                 positive_row = (self.df.loc[self.df['vehicleID'] == car_A]).sample()
                 car_P, name_P = positive_row['vehicleID'].values[0], positive_row['imageName'].values[0]
-                
+
                 # Load car ID and image name for negative
                 negative_row = (self.df.loc[self.df['vehicleID'] != car_A]).sample()
                 car_N, name_N = negative_row['vehicleID'].values[0], negative_row['imageName'].values[0]
 
-                img_A = self.load_img(dirc+name_A)
-                img_P = self.load_img(dirc+name_P)
-                img_N = self.load_img(dirc+name_N)
+                img_A = self.load_img(dirc + name_A)
+                img_P = self.load_img(dirc + name_P)
+                img_N = self.load_img(dirc + name_N)
 
                 if random.choice([True, False]):
                     label = [1]
@@ -94,21 +88,3 @@ class MyGenerator():
             label_train = np.array(labels)
 
             yield [pair_train[:, 0], pair_train[:, 1]], label_train[:]
-
-    def dataCarGenerator(self):
-        X1 = self.anchor
-        X2 = self.positive
-        X3 = self.negative
-        Y = self.Y_train
-        b = self.batch
-
-    # Funkcia prebrata z https://github.com/noelcodella/tripletloss-keras-tensorflow/blob/master/tripletloss.py
-        local_seed = T_G_SEED
-        genX1 = self.datagen.flow(X1,Y, batch_size=b, seed=local_seed, shuffle=False)
-        genX2 = self.datagen.flow(X2,Y, batch_size=b, seed=local_seed, shuffle=False)
-        genX3 = self.datagen.flow(X3,Y, batch_size=b, seed=local_seed, shuffle=False)
-        while True:
-                X1i = genX1.next()
-                X2i = genX2.next()
-                X3i = genX3.next()
-                return [X1i[0], X2i[0], X3i[0]], X1i[1]
